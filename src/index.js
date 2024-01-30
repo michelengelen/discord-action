07c6993487430bde939a9af7782a11a69b802c98
michel <jsnerdic@gmail.com>
2024-01-30 10:43:19 +0100

    adjustments

--- src/index.js
+++ src/index.js
@@ -1,85 +1,87 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
 const axios = require('axios');
 const fs = require('fs');
 const _ = require('lodash');
-const yargs = require('yargs/yargs')
-const { hideBin } = require('yargs/helpers')
-const argv = yargs(hideBin(process.argv)).argv
 
-const REQUIRED_ENV_VARS = [
-	'GITHUB_EVENT_PATH',
-	'GITHUB_REPOSITORY',
-	'GITHUB_WORKFLOW',
-	'GITHUB_ACTOR',
-	'GITHUB_EVENT_NAME',
-	'GITHUB_ACTION',
-	'DISCORD_WEBHOOK',
-];
+function main() {
+	try {
+		/**
+		 * We need to fetch all the inputs that were provided to our action
+		 * and store them in variables for us to use.
+		 **/
+		const webhookUrl = core.getInput('webhook-url', { required: true });
+		const repo = core.getInput('repo', { required: true });
+		const pr_number = core.getInput('pr_number', { required: true });
+		const token = core.getInput('token', { required: true });
 
-process.env.GITHUB_ACTION = process.env.GITHUB_ACTION || '<missing GITHUB_ACTION env var>';
+		const payload = github.context.payload;
 
-REQUIRED_ENV_VARS.forEach(env => {
-	if (!process.env[env] || !process.env[env].length) {
-		console.error(`Env var ${env} is not defined. Maybe try to set it if you are running the script manually.`);
-		process.exit(1);
+		console.log('payload', payload)
 	}
-});
-
-const eventContent = fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8');
-
-_.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
-
-let url;
-let payload;
-
-if (argv._.length === 0 && !process.env.DISCORD_EMBEDS) {
-	// If argument and embeds NOT provided, let Discord show the event information.
-	url = `${process.env.DISCORD_WEBHOOK}/github`;
-	payload = JSON.stringify(JSON.parse(eventContent));
-} else {
-	// Otherwise, if the argument or embeds are provided, let Discord override the message.
-	const args = argv._.join(' ');
-	const message = _.template(args)({
-		...process.env,
-		EVENT_PAYLOAD: JSON.parse(eventContent),
-	});
-
-	let embedsObject;
-	if (process.env.DISCORD_EMBEDS) {
-		try {
-			embedsObject = JSON.parse(process.env.DISCORD_EMBEDS);
-		} catch (parseErr) {
-			console.error('Error parsing DISCORD_EMBEDS :' + parseErr);
-			process.exit(1);
-		}
+	catch (error) {
+		core.setFailed(error.message);
 	}
-
-	url = process.env.DISCORD_WEBHOOK;
-	payload = JSON.stringify({
-		content: message,
-		...(process.env.DISCORD_EMBEDS && {embeds: embedsObject}),
-		...(process.env.DISCORD_USERNAME && {
-			username: process.env.DISCORD_USERNAME,
-		}),
-		...(process.env.DISCORD_AVATAR && {
-			avatar_url: process.env.DISCORD_AVATAR,
-		}),
-	});
 }
 
-// Curl -X POST -H "Content-Type: application/json" --data "$(cat $GITHUB_EVENT_PATH)" $DISCORD_WEBHOOK/github
+main()
 
-(async () => {
-	console.log('Sending message ...');
-	await axios.post(`${url}?wait=true`, payload, {
-		headers: {
-			'Content-Type': 'application/json',
-			'X-GitHub-Event': process.env.GITHUB_EVENT_NAME,
-		},
-	});
-	console.log('Message sent ! Shutting down ...');
-	process.exit(0);
-})().catch(err => {
-	console.error('Error :', err.response.status, err.response.statusText);
-	console.error('Message :', err.response ? err.response.data : err.message);
-	process.exit(1);
-});
+// const eventContent = fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8');
+//
+// _.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
+//
+// let url;
+// let payload;
+//
+// if (argv._.length === 0 && !process.env.DISCORD_EMBEDS) {
+// 	// If argument and embeds NOT provided, let Discord show the event information.
+// 	url = `${process.env.DISCORD_WEBHOOK}/github`;
+// 	payload = JSON.stringify(JSON.parse(eventContent));
+// } else {
+// 	// Otherwise, if the argument or embeds are provided, let Discord override the message.
+// 	const args = argv._.join(' ');
+// 	const message = _.template(args)({
+// 		...process.env,
+// 		EVENT_PAYLOAD: JSON.parse(eventContent),
+// 	});
+//
+// 	let embedsObject;
+// 	if (process.env.DISCORD_EMBEDS) {
+// 		try {
+// 			embedsObject = JSON.parse(process.env.DISCORD_EMBEDS);
+// 		} catch (parseErr) {
+// 			console.error('Error parsing DISCORD_EMBEDS :' + parseErr);
+// 			process.exit(1);
+// 		}
+// 	}
+//
+// 	url = process.env.DISCORD_WEBHOOK;
+// 	payload = JSON.stringify({
+// 		content: message,
+// 		...(process.env.DISCORD_EMBEDS && {embeds: embedsObject}),
+// 		...(process.env.DISCORD_USERNAME && {
+// 			username: process.env.DISCORD_USERNAME,
+// 		}),
+// 		...(process.env.DISCORD_AVATAR && {
+// 			avatar_url: process.env.DISCORD_AVATAR,
+// 		}),
+// 	});
+// }
+//
+// // Curl -X POST -H "Content-Type: application/json" --data "$(cat $GITHUB_EVENT_PATH)" $DISCORD_WEBHOOK/github
+//
+// (async () => {
+// 	console.log('Sending message ...');
+// 	await axios.post(`${url}?wait=true`, payload, {
+// 		headers: {
+// 			'Content-Type': 'application/json',
+// 			'X-GitHub-Event': process.env.GITHUB_EVENT_NAME,
+// 		},
+// 	});
+// 	console.log('Message sent ! Shutting down ...');
+// 	process.exit(0);
+// })().catch(err => {
+// 	console.error('Error :', err.response.status, err.response.statusText);
+// 	console.error('Message :', err.response ? err.response.data : err.message);
+// 	process.exit(1);
+// });
